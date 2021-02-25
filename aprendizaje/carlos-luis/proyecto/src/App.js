c8aee9c22722f8e0d4f7ebdec71c34beda2c11cc
Carlos Valencia <rasfreed@gmail.com>
2021-02-25 18:13:41 -0500

    creating loginForm class

--- aprendizaje/carlos-luis/proyecto/src/App.js
+++ aprendizaje/carlos-luis/proyecto/src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+class loginForm extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+      <form>
+          <p>Sign in</p>
+          <hr/>
+          <input/>
+      </form>
+    )
+  }
+}
+
 function App() {
   return (
     
-  );
+  )
 }
 
 export default App;
